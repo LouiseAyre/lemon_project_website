bfc5b99fa5a8187842ba8664fa4792c0a3dfc556
Ling Chin <ling_chin99@yahoo.com>
2022-09-01 22:33:19 +1000

    front end

--- app.py
+++ app.py
@@ -1,29 +1,54 @@
 import streamlit as st
 import requests
+import base64
 
-st.markdown("Lemon Health Assessment")
+# url='http://localhost:8000/predict' #this is a local api
+url ="https://lemonprojectapi-tjtjmdtcna-de.a.run.app/predict"
 
-new_image = st.file_uploader("Upload a jpg file", type=".jpg", accept_multiple_files=False)
-if new_image is not None:
-    st.write("uploaded jpg file...")
-    st.image(new_image)
+lemon_background = "https://storage.googleapis.com/wagon-data-852-chin/lemon_background.png"
+lemon_bg = "https://storage.googleapis.com/wagon-data-852-chin/lemon_bg.png"
 
-    url='http://localhost:8000/predict' #this is a local api
+page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("https://storage.googleapis.com/wagon-data-852-chin/lemon_background.png");
+    background-size: cover;
+    }
+    </style>
+    '''
 
+st.write(page_bg_img, unsafe_allow_html=True)
+
+# set_png_as_page_bg('lemon_background.png')
+# st.markdown(set_png_as_page_bg('lemon_background.png'), unsafe_allow_html=True)
+
+def lemon_health(new_image):
     files = {"file": (new_image.name, new_image, "multipart/form-data")}
-    # response = {'health':0.5}
     response = requests.post(url,files=files).json()
-    print("response", type(response), response.keys(), response['health'])
     health_rating = response['health']
 
-    if health_rating <0.3:
-        health = "Fairly healthy"
-    elif health_rating >=0.3 and health_rating <= 0.6:
-        health = "Not very healthy"
+    if health_rating > 0.9:
+        health = "Unhealthy"
     else:
-        health= "Unhealthy!"
+        health = "Healthy"
+    return health
 
-    st.write("health of lemon:", health)
+st.markdown("**Lemon Health Assessment**")
 
+direction = st.radio('Select an option',('upload jpg','take a picture'))
+
+
+if direction == 'upload jpg':
+    new_image = st.file_uploader("Upload jpg", type=".jpg", accept_multiple_files=False)
+else:
+    direction == 'take a picture'
+    new_image = st.camera_input("Take a picture")
+
+
+if new_image is not None:
+    st.write("image uploaded")
+    st.image(new_image, width=300)
+    health_rating = lemon_health(new_image)
+    st.write("health of lemon:", health_rating)
 else:
     st.write("please upload image")
